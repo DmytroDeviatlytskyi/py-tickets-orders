a4ea32cb49bbe43efad78a5360c6af3ca45475b5
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-07-11 18:44:34 +0200

    delete import & Add ticket validation to serializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -107,6 +107,16 @@ class TicketSerializer(serializers.ModelSerializer):
         model = Ticket
         fields = ("id", "row", "seat", "movie_session")
 
+        def validate(self, attrs):
+            data = super(TicketSerializer, self).validate(attrs)
+            Ticket.validate_ticket(
+                attrs["row"],
+                attrs["seat"],
+                attrs["movie_session"].cinema_hall,
+                serializers.ValidationError
+            )
+            return data
+
 
 class TicketListSerializer(TicketSerializer):
     movie_session = MovieSessionListSerializer(many=False, read_only=True)
